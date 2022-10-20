3fd1cd9cf732455d787890162b2eaa0ead841533
Jesse Maarleveld <jessemaarleveld@gmail.com>
2022-10-20 08:43:59 +0200

    Update show_tree

--- pygll/util/show_tree.py
+++ pygll/util/show_tree.py
@@ -8,50 +8,7 @@ from ..core import base as _base
 try:
     import graphviz
 except ImportError:
-    graphviz = None
-
-##############################################################################
-##############################################################################
-# Textual Printing of trees
-##############################################################################
-
-
-def print_tree(node: _base.IntermediateNode):
-    _print_recursive(node)
-
-
-def _print_recursive(node: _base.Node, *, indent=0):
-    match node:
-        case _base.IntermediateNode(left_extent=_,
-                                    right_extent=_,
-                                    slot=slot,
-                                    children=children):
-            _print(f'INode<{slot}>', indent=indent)
-            for child in children.values():
-                _print('Alternative:', indent=indent + 1)
-                _print_recursive(child, indent=indent + 2)
-        case _base.TerminalNode(left_extent=_,
-                                right_extent=_,
-                                symbol=symbol):
-            _print(f'TNode<{symbol}>', indent=indent)
-        case _base.PackedNode(left_extent=_,
-                              right_extent=_,
-                              split=_,
-                              slot=slot,
-                              left=left,
-                              right=right):
-            _print(f'PNode<{slot}>', indent=indent)
-            if left is not None:
-                _print('Left:', indent=indent + 1)
-                _print_recursive(left, indent=indent + 2)
-            _print('Right:', indent=indent + 1)
-            _print_recursive(right, indent=indent + 2)
-        case _:
-            raise ValueError(node)
-
-
-def _print(line: str, indent: int):
-    print('    '*indent + line)
+    raise RuntimeError('Tree visualization requires graphviz')
 
 
 ##############################################################################
@@ -60,7 +17,7 @@ def _print(line: str, indent: int):
 ##############################################################################
 
 
-def show_tree_raw(tree: _base.Node):
+def show_tree(tree: _base.Node):
     dot = graphviz.Digraph()
     _show_recursive_raw(tree, dot, None, set(), set())
     dot.render('graph.gv', view=True)
@@ -83,10 +40,9 @@ def _show_recursive_raw(node: _base.Node,
         case _base.IntermediateNode(left_extent=left,
                                     right_extent=right,
                                     slot=slot,
-                                    is_nonterminal_node=is_nonterminal,
                                     children=children):
             key = str(id(node))
-            if is_nonterminal:
+            if slot.alternate < 0:
                 label = f'NNode<{slot.nonterminal}, {left}, {right}>'
             else:
                 label = f'INode<{slot}, {left}, {right}>'
